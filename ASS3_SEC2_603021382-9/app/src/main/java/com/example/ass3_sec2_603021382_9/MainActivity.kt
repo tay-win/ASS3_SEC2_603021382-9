fee090dd722bc5096e31997a20c745a6e1f878c3
lukka-na <lukkana.p@kkumail.com>
2019-08-15 22:21:01 +0700

    ASS3_SEC2_603021382-9

---
+++ ASS3_SEC2_603021382-9/app/src/main/java/com/example/ass3_sec2_603021382_9/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ass3_sec2_603021382_9
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
